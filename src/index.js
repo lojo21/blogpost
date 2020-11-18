b56c9b58ee9bca3c6b828de0b3cae9702f007e18
Josue Lorenzo <josuelorenzo21@gmail.com>
2020-11-18 14:42:15 -0300

    starting to connect reduxThunk

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import reactDOM from "react-dom";
+import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
 import {createStore} from "redux";
 
-import App from ".components/App";
+import App from "./components/App"
 import reducers from "./reducers";
 
 ReactDOM.render(
@@ -11,4 +11,4 @@ ReactDOM.render(
         <App/>
     </Provider>,
     document.querySelector("#root")
-)
+);
